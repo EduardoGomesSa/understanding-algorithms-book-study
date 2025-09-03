57580821bc64ba6f1b4debc0f7230abf8a5a628f
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-09-03 08:52:04 -0300

    Hello world in Rust

---
+++ rust/hello-rust/target/debug/deps/hello_rust-b2d0ff2ab9ad95fa.d
@@ -0,0 +1,5 @@
+/home/eduardo/Projetos/hello-rust/target/debug/deps/hello_rust-b2d0ff2ab9ad95fa.d: src/main.rs
+
+/home/eduardo/Projetos/hello-rust/target/debug/deps/hello_rust-b2d0ff2ab9ad95fa: src/main.rs
+
+src/main.rs:
---
+++ rust/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/eduardo/Projetos/php/understanding-algorithms-book-study/rust/hello-rust/target/debug/hello-rust: /home/eduardo/Projetos/php/understanding-algorithms-book-study/rust/hello-rust/src/main.rs
